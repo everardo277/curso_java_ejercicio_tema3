82e44a2e057f3604d9dd4ec12b47f586f170c28f
everardo277 <rene.contrerasari@nube.unadmexico.mx>
2022-07-06 13:42:35 -0500

    Primer commit

---
+++ src/com/ejercicios/EjercicioTema3.java
@@ -0,0 +1,15 @@
+package com.ejercicios;
+
+public class EjercicioTema3 {
+    public static void main(String[] args) {
+
+        String[] colores = {"Azul","Rojo", "Verde",  "Amarillo", "Gris", "Cafe", "Blanco","Negro"};
+        String titulo = "Los colores principales son:" ;
+        System.out.println(titulo);
+        for (int i = 0; i < colores.length; i++){
+            System.out.println(" El color :" + colores[i]);
+        }
+
+    }
+
+}
